eae63f3a40dc1ab9bd448143744a9254fdf5b59d
Dar9586 <atoppi2012@gmail.com>
2017-02-23 22:48:13 +0100

    0.0.5

--- execute.php
+++ execute.php
@@ -7,12 +7,12 @@ if(!$update)
 }
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
-$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
-$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
-$date = isset($message['date']) ? $message['date'] : "";
-$text = isset($message['text']) ? $message['text'] : "";
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";//id chat
+$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";//nome
+$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";//cognome
+$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";//nick
+$date = isset($message['date']) ? $message['date'] : "";//data
+$text = isset($message['text']) ? $message['text'] : "";//testo
 $text = trim($text);
 $text = strtolower($text);
 header("Content-Type: application/json");
