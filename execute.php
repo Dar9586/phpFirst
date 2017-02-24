4e54bded6d4abe33edba38242eeba4cc1e05591c
Dar9586 <atoppi2012@gmail.com>
2017-02-24 15:57:52 +0100

    0.0.6

--- execute.php
+++ execute.php
@@ -1,4 +1,35 @@
-<?php
+﻿<?php
+function receivedText(){
+	$response = "Ho ricevuto un messaggio di testo";
+}
+function receivedAudio(){
+	$response = "Ho ricevuto un messaggio audio";
+}
+function receivedDocument(){
+	$response = "Ho ricevuto un messaggio document";
+}
+function receivedPhoto(){
+	$response = "Ho ricevuto un messaggio photo";
+}
+function receivedSticker(){
+	$response = "Ho ricevuto un messaggio sticker";
+}
+function receivedVideo(){
+	$response = "Ho ricevuto un messaggio video";
+}
+function receivedVoice(){
+	$response = "Ho ricevuto un messaggio voice";
+}
+function receivedContact(){
+	$response = "Ho ricevuto un messaggio contact";
+}
+	$response = "Ho ricevuto un messaggio location";
+}
+	$response = "Ho ricevuto un messaggio venue";
+}
+function receivedOther(){
+	$response = "Ho ricevuto un messaggio other";
+}
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 if(!$update)
@@ -6,7 +37,7 @@ if(!$update)
   exit;
 }
 $message = isset($update['message']) ? $update['message'] : "";
-$messageId = isset($message['message_id']) ? $message['message_id'] : "";
+$messageId = isset($message['message_id']) ? $message['message_id'] : "";//numero messaggio(parte da 0)
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";//id chat
 $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";//nome
 $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";//cognome
@@ -17,21 +48,39 @@ $text = trim($text);
 $text = strtolower($text);
 header("Content-Type: application/json");
 $response = '';
-if(strpos($text, "/start") === 0 || $text=="ciao")
-{
-	$response = "$message\n$messageId\n$chatId\n$firstname\n$lastname\n$username\n$date\n$text";
+if(isset($message['text'])){
+	receivedText();
+	
 }
-elseif($text=="domanda 1")
-{
-	$response = "risposta 1";
+elseif(isset($message['audio'])){
+	receivedAudio();
 }
-elseif($text=="domanda 2")
-{
-	$response = "risposta 2";
+elseif(isset($message['document'])){
+	receivedDocument();
 }
-else
-{
-	$response = "Comando non valido!";
+elseif(isset($message['photo'])){
+	receivedPhoto();
+}
+elseif(isset($message['sticker'])){
+	receivedSticker();
+}
+elseif(isset($message['video'])){
+	receivedVideo();
+}
+elseif(isset($message['voice'])){
+	receivedVoice();
+}
+elseif(isset($message['contact'])){
+	receivedContact();
+}
+elseif(isset($message['location'])){
+	receivedLocation();
+}
+elseif(isset($message['venue'])){
+	receivedVenue();
+}
+else{
+	receivedOther();
 }
 $parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
